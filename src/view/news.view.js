a743d50b2bf1f87fbd1ae0a2ab72f2e10d0040d6
zidein07 <zidein07@gmail.com>
2015-12-19 22:54:51 +0400

    yeha i geting data and draw them

--- src/view/news.view.js
+++ src/view/news.view.js
@@ -4,13 +4,33 @@ var Backbone = Backbone || {};
 app.newsView = Backbone.View.extend({
   el: 'body',
   initialize: function () {
-
+    
   },
   render: function () {
     var self = this;
+    var res = '';
     this.data = [];
-    $.get("https://hacker-news.firebaseio.com/v0/topstories.json", function (data) {
-      self.data = data;
+    $.when($.ajax("https://hacker-news.firebaseio.com/v0/topstories.json")).then(function (item) {
+      self.data = item;
+      self.data.forEach(function (item, i, arr) {
+        $.get('https://hacker-news.firebaseio.com/v0/item/' + item + '.json', function (item) {
+          console.log('obj', item);
+          res = self.templateListData({title: item.title, link: item.url});
+          $('.listPost').append(res);
+        });
+      });
+    }, function (error) {
+      console.log('Cant get data error ' + error);
+      return false;
     });
+
+
+
+  },
+  templateListData: function (context) {
+    var source = $("#postListItemTpl").html();
+    var template = Handlebars.compile(source);
+    return template(context);
   }
-});
+})
+;
