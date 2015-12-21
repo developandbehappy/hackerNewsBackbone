ad8b0aaa9b8723e7b51e17a2654b58bfb8257d86
zidein07 <zidein07@gmail.com>
2015-12-21 19:09:07 +0400

    few draw comments but need create tree

--- src/view/news.view.js
+++ src/view/news.view.js
@@ -13,7 +13,7 @@ app.newsView = Backbone.View.extend({
   render: function () {
     var self = this;
     this.data = [];
-      $.when($.ajax("https://hacker-news.firebaseio.com/v0/topstories.json")).then(function (items) {
+    $.when($.ajax("https://hacker-news.firebaseio.com/v0/topstories.json")).then(function (items) {
       self.data = items;
       return self.getPromiseArray(0, self.defaultLimit, items);
     }, function (error) {
--- src/view/newsItem.view.js
+++ src/view/newsItem.view.js
@@ -5,11 +5,13 @@ app.newsItem = Backbone.View.extend({
   initialize: function () {
 
   },
-  render: function (id, comment) {
+  render: function (id) {
     var self = this;
     self.clearContent();
     $.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json').then(function (response) {
+      console.log('response', response);
       self.renderItemData(response);
+      self.renderComment(response);
     }, function (error) {
       console.log('error', error);
     });
@@ -22,27 +24,52 @@ app.newsItem = Backbone.View.extend({
     var template = Handlebars.compile(source);
     return template(context);
   },
-  templateItemComment: function (context) {
+  templateCommentItemData: function (context) {
     var source = $("#postItemComment").html();
     var template = Handlebars.compile(source);
     return template(context);
   },
-  getContent: function () {
-    return $('.content');
+
+  templateItemComment: function (context) {
+    var html = '';
+    var self = this;
+    context.forEach(function (item) {
+      var data = item[0];
+//      console.log('data', data);
+      var result = self.templateCommentItemData(data);
+      console.log('result', result);
+      html = html + result;
+    });
+
+    return html;
   },
   renderItemData: function (item) {
     var itemData = this.templateItemData(item);
-    this.responseArray(item.kids);
-    var html = itemData; // Рендер html
-    this.getContent().html(html);
+    $('.content').html(itemData);
+//    app.events.trigger('loaderHide');
+    return itemData;
+  },
+  renderListComments(arg) {
+    console.log('arg', arg);
+    var itemData = this.templateItemComment(arg);
+    console.log('itemData', itemData);
+    $('.commentsWrapper').append(itemData);
     app.events.trigger('loaderHide');
   },
-  renderComment: function (itemComment) {
-    console.log(itemComment);
-  },
-  responseArray: function (item) {
-    item.promise().done(function (argOne) {
-      console.log('argOne', argOne);
+  renderComment: function (itemNews) {
+    var deferredsList = [];
+    var self = this;
+    console.log('itemNews', itemNews);
+    itemNews.kids.map(function (it) {
+      var iii = $.get('https://hacker-news.firebaseio.com/v0/item/' + it + '.json');
+      deferredsList.push(iii);
+    });
+    return $.when.apply($, deferredsList).then(function () {
+      var args = Array.prototype.slice.call(arguments);
+      self.renderListComments(args);
+    }, function () {
+      console.log('Cant download data from listItems');
+      return false;
     });
   }
 });
